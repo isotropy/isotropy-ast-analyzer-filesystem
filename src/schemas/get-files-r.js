83a88a8901a21b68e5f69b2e1ecb7bac6abac023
arivanandan <uarivanandan@gmail.com>
2017-08-06 12:33:03 +0530

    Remove get-files-r equality check

--- src/schemas/get-files-r.js
+++ src/schemas/get-files-r.js
@@ -102,31 +102,22 @@ export default function(state, analysisState) {
         return result instanceof Match
           ? (() => {
               const fs = result.value.fs;
-              debugger;
-              return R.equals(
-                clean(result.value.args[0].body.dirNode1),
-                clean(result.value.args[0].body.arguments[0])
-              )
-                ? result.value.args[0].params[0].fsIdentifier1 &&
-                    result.value.args[0].body.fsIdentifier2 ===
-                      result.value.args[0].body.fsIdentifier3
-                  ? getFiles(
-                      {
-                        dir: clean(result.value.args[0].body.dirNode1),
-                        recurse: true
-                      },
-                      {
-                        identifier: fs.identifier,
-                        module: fs.module,
-                        collection: fs.collection
-                      }
-                    )
-                  : new Skip(
-                      `The fs access identifier should be the same across all instances.`
-                    )
+              return result.value.args[0].params[0].fsIdentifier1 &&
+                result.value.args[0].body.fsIdentifier2 ===
+                  result.value.args[0].body.fsIdentifier3
+                ? getFiles(
+                    {
+                      dir: clean(result.value.args[0].body.dirNode1),
+                      recurse: true
+                    },
+                    {
+                      identifier: fs.identifier,
+                      module: fs.module,
+                      collection: fs.collection
+                    }
+                  )
                 : new Skip(
-                    `The directory names should be the same to perform a
-            recursive traversal to list all files.`
+                    `The fs access identifier should be the same across all instances.`
                   );
             })()
           : result;
