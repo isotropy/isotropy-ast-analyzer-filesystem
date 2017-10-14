01acc5beadc6c084366b92c1264e03ccbeaace64
arivanandan <uarivanandan@gmail.com>
2017-10-14 17:20:16 +0530

    Better exit messages from analyze-meta

--- src/analyze-meta.js
+++ src/analyze-meta.js
@@ -4,26 +4,31 @@ export default function(analysisState) {
   return {
     analyzeImportDeclaration(babelPath, state) {
       // Incorrect config
-      if (!state.opts.projects) return false;
+      if (!state.opts.projects)
+        return {
+          error:
+            "We don't understand your configuration! Please check your .babelrc."
+        };
 
       const fsProject = state.opts.projects.find(project => {
         const projectDir = project.dir.startsWith("./")
           ? project.dir
           : "./" + project.dir;
-        const absolutePath = path.resolve(projectDir) + "/";
+        const absolutePath = path.resolve(projectDir);
         return state.file.opts.filename.startsWith(absolutePath);
       });
 
       // Not a fs project
-      if (!fsProject) return false;
+      if (!fsProject)
+        return {
+          skip: "This folder is not specified for transpilation in the config."
+        };
 
       const moduleName = babelPath.get("source").node.value;
       const resolvedName = path.resolve(
         path.dirname(state.file.opts.filename),
         moduleName
       );
-
-      debugger;
 
       const fsModule = fsProject.modules.find(m => {
         const sourceDir = m.source.startsWith("./")
@@ -33,7 +38,8 @@ export default function(analysisState) {
         return absolutePath === resolvedName;
       });
 
-      if (!fsModule) return false;
+      if (!fsModule)
+        return { skip: "This import is not what we are looking for." };
 
       const specifier = babelPath.get("specifiers.0").node.local.name;
       analysisState.importBindings = analysisState.importBindings.concat({
